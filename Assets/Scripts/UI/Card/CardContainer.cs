0c18bf0257c62ff72a8ae73bf44085a3ce946731
methiy <3250949283@qq.com>
2024-08-18 12:54:11 +0800

    卡牌的UI效果

--- Assets/Scripts/UI/Card/CardContainer.cs
+++ Assets/Scripts/UI/Card/CardContainer.cs
@@ -1,8 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardContainer : MonoBehaviour
 {
-    [SerializeField] CardTemplate cardTemplate;
+    private List<CardTemplate>  cards = new List<CardTemplate>();
+
+    private void Start()
+    {
+        InitCardList();
+    }
+
+    private void InitCardList(){
+        cards.Clear();
+        foreach (Transform child in transform)
+        {
+            CardTemplate card = child.GetComponent<CardTemplate>();
+            if (card != null)
+            {
+                cards.Add(card);
+            }
+        }
+    }
+
+    public void SetCardPreParent(Transform parent){
+        for (int i = 0; i < cards.Count; i++){
+            cards[i].transform.SetParent(parent);
+        }
+        InitCardList();
+        UpdateCardsID();
+    }
+    public void SetCardTempParent(int index,Transform parent){
+        for (int i = index; i < cards.Count; i++){
+            cards[i].transform.SetParent(parent);
+        }
+    }
+
+
+    public void UpdateCardsID(){
+        int index = 0;
+        foreach (var card in cards)
+        {
+            SetCardsID(card,index);
+            index++;
+        }
+    }
+    public void SetCardsID(CardTemplate card, int index){
+        card.SetID(index);
+    }
+
 }
