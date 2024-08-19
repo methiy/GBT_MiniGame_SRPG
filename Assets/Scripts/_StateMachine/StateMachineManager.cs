d03155d9199138edc7270c41755522dd939dfc2e
AllToBeNize <2184196946@qq.com>
2024-08-19 17:26:12 +0800

    修改StateMachine引用

--- Assets/Scripts/_StateMachine/StateMachineManager.cs
+++ Assets/Scripts/_StateMachine/StateMachineManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditorInternal.VersionControl.ListControl;
 
 public class StateMachineManager : MonoBehaviour
 {
